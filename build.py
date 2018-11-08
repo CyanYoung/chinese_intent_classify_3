0674a472663959349b87ee0a2d44a46ffab46a4b
yangjc <bupt_yjc@163.com>
2018-11-08 19:35:23 +0800

    add device

--- build.py
+++ build.py
@@ -1,3 +1,5 @@
+import time
+
 import pickle as pk
 
 import torch
@@ -29,6 +31,8 @@ paths = {'dnn': 'model/dnn.pkl',
          'cnn': 'model/cnn.pkl',
          'rnn': 'model/rnn.pkl'}
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 
 def load_feat(path_feats):
     with open(path_feats['sent_train'], 'rb') as f:
@@ -46,7 +50,7 @@ def tensorize(path_feats):
     feats = load_feat(path_feats)
     tensors = list()
     for feat in feats:
-        tensors.append(torch.LongTensor(feat))
+        tensors.append(torch.LongTensor(feat).to(device))
     return tensors
 
 
@@ -63,6 +67,16 @@ def get_metric(model, loss_func, sents, labels):
     return loss, acc
 
 
+def step_print(step, batch_loss, batch_acc):
+    print('\n{} {} - loss: {:.3f} - acc: {:.3f}'.format('step', step + 1, batch_loss, batch_acc))
+
+
+def epoch_print(epoch, delta, train_loss, train_acc, dev_loss, dev_acc, extra):
+    basic = '\n{} {} - {:.2f}s - loss: {:.3f} - acc: {:.3f} - val_loss: {:.3f} - val_acc: {:.3f}'.format(
+            'epoch', epoch + 1, delta, train_loss, train_acc, dev_loss, dev_acc)
+    print(basic + extra)
+
+
 def fit(name, epoch, embed_mat, class_num, path_feats, detail):
     train_sents, train_labels, dev_sents, dev_labels = tensorize(path_feats)
     train_loader = get_loader(train_sents, train_labels)
@@ -70,11 +84,13 @@ def fit(name, epoch, embed_mat, class_num, path_feats, detail):
     seq_len = len(train_sents[0])
     arch = map_item(name, archs)
     model = arch(embed_mat, seq_len, class_num)
+    model.to(device)
     loss_func = CrossEntropyLoss()
     optimizer = Adam(model.parameters(), lr=1e-3)
     min_dev_loss = float('inf')
     print('\n{}'.format(model))
     for i in range(epoch):
+        start = time.time()
         model.train()
         for step, (sent_batch, label_batch) in enumerate(train_loader):
             batch_loss, batch_acc = get_metric(model, loss_func, sent_batch, label_batch)
@@ -82,19 +98,18 @@ def fit(name, epoch, embed_mat, class_num, path_feats, detail):
             batch_loss.backward()
             optimizer.step()
             if detail:
-                print('\n%s %d - loss: %.3f - acc: %.3f' % ('step', step + 1, batch_loss, batch_acc))
+                step_print(step, batch_loss, batch_acc)
+        delta = time.time() - start
         with torch.no_grad():
             model.eval()
             train_loss, train_acc = get_metric(model, loss_func, train_sents, train_labels)
             dev_loss, dev_acc = get_metric(model, loss_func, dev_sents, dev_labels)
-        info = '\n{} {} - loss: {:.3f} - acc: {:.3f} - val_loss: {:.3f} - val_acc: {:.3f}, '.format(
-               'epoch', i + 1, train_loss, train_acc, dev_loss, dev_acc)
+        extra = ''
         if dev_loss < min_dev_loss:
-            print(info + 'val_loss reduced by {:.3f}'.format(min_dev_loss - dev_loss, name))
+            extra = ', val_loss reduce {:.3f}'.format(min_dev_loss - dev_loss, name)
             torch.save(model, map_item(name, paths))
             min_dev_loss = dev_loss
-        else:
-            print(info + 'val_loss not reduced')
+        epoch_print(i, delta, train_loss, train_acc, dev_loss, dev_acc, extra)
 
 
 if __name__ == '__main__':
