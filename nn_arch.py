704fa3f66d516429e11a067978cf375acfbebddb
yangjc <bupt_yjc@163.com>
2018-11-15 15:47:12 +0800

    change name

--- nn_arch.py
+++ nn_arch.py
@@ -70,6 +70,6 @@ class Rnn(nn.Module):
 
     def forward(self, x):
         x = self.embed(x)
-        x, state = self.ra(x)
+        x, h = self.ra(x)
         x = x[:, -1, :]
         return self.ln(x)
