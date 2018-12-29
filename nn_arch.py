049d52205c1a6cba4332205a44db5f5fabe5a250
yangjc <bupt_yjc@163.com>
2018-12-29 11:28:35 +0800

    simplify init

--- nn_arch.py
+++ nn_arch.py
@@ -8,15 +8,14 @@ seq_len = 30
 class Dnn(nn.Module):
     def __init__(self, embed_mat, class_num):
         super(Dnn, self).__init__()
-        self.vocab_num, self.embed_len = embed_mat.size()
-        self.class_num = class_num
-        self.embed = nn.Embedding(self.vocab_num, self.embed_len, _weight=embed_mat)
-        self.la1 = nn.Sequential(nn.Linear(self.embed_len, 200),
+        vocab_num, embed_len = embed_mat.size()
+        self.embed = nn.Embedding(vocab_num, embed_len, _weight=embed_mat)
+        self.la1 = nn.Sequential(nn.Linear(embed_len, 200),
                                  nn.ReLU())
         self.la2 = nn.Sequential(nn.Linear(200, 200),
                                  nn.ReLU())
         self.dl = nn.Sequential(nn.Dropout(0.2),
-                                nn.Linear(200, self.class_num))
+                                nn.Linear(200, class_num))
 
     def forward(self, x):
         x = self.embed(x)
@@ -29,22 +28,21 @@ class Dnn(nn.Module):
 class Cnn(nn.Module):
     def __init__(self, embed_mat, class_num):
         super(Cnn, self).__init__()
-        self.vocab_num, self.embed_len = embed_mat.size()
-        self.class_num = class_num
-        self.embed = nn.Embedding(self.vocab_num, self.embed_len, _weight=embed_mat)
-        self.cap1 = nn.Sequential(nn.Conv1d(self.embed_len, 64, kernel_size=1, padding=0),
+        vocab_num, embed_len = embed_mat.size()
+        self.embed = nn.Embedding(vocab_num, embed_len, _weight=embed_mat)
+        self.cap1 = nn.Sequential(nn.Conv1d(embed_len, 64, kernel_size=1, padding=0),
                                   nn.ReLU(),
                                   nn.MaxPool1d(seq_len))
-        self.cap2 = nn.Sequential(nn.Conv1d(self.embed_len, 64, kernel_size=2, padding=1),
+        self.cap2 = nn.Sequential(nn.Conv1d(embed_len, 64, kernel_size=2, padding=1),
                                   nn.ReLU(),
                                   nn.MaxPool1d(seq_len + 1))
-        self.cap3 = nn.Sequential(nn.Conv1d(self.embed_len, 64, kernel_size=3, padding=1),
+        self.cap3 = nn.Sequential(nn.Conv1d(embed_len, 64, kernel_size=3, padding=1),
                                   nn.ReLU(),
                                   nn.MaxPool1d(seq_len))
         self.la = nn.Sequential(nn.Linear(192, 200),
                                 nn.ReLU())
         self.dl = nn.Sequential(nn.Dropout(0.2),
-                                nn.Linear(200, self.class_num))
+                                nn.Linear(200, class_num))
 
     def forward(self, x):
         x = self.embed(x)
@@ -61,12 +59,11 @@ class Cnn(nn.Module):
 class Rnn(nn.Module):
     def __init__(self, embed_mat, class_num):
         super(Rnn, self).__init__()
-        self.vocab_num, self.embed_len = embed_mat.size()
-        self.class_num = class_num
-        self.embed = nn.Embedding(self.vocab_num, self.embed_len, _weight=embed_mat)
-        self.ra = nn.LSTM(self.embed_len, 200, batch_first=True)
+        vocab_num, embed_len = embed_mat.size()
+        self.embed = nn.Embedding(vocab_num, embed_len, _weight=embed_mat)
+        self.ra = nn.LSTM(embed_len, 200, batch_first=True)
         self.dl = nn.Sequential(nn.Dropout(0.2),
-                                nn.Linear(200, self.class_num))
+                                nn.Linear(200, class_num))
 
     def forward(self, x):
         x = self.embed(x)
